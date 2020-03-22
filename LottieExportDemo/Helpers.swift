dd7f1ba7316808b57f0daa2492c83c4895d03672
q210 <q210.frk@gmail.com>
2020-03-22 14:56:58 +0300

    reformat code using swiftformat 0.44.6

--- LottieExportDemo/Helpers.swift
+++ LottieExportDemo/Helpers.swift
@@ -6,18 +6,17 @@
 //  Copyright © 2019 Damik Minnegalimov. All rights reserved.
 //
 
-import Foundation
-import UIKit
 import AVKit
+import Foundation
 import Photos
-
+import UIKit
 
 func playVideo(url: URL) {
     DispatchQueue.main.async {
         let player = AVPlayer(url: url)
         let playerViewController = AVPlayerViewController()
         playerViewController.player = player
-        
+
         let viewController = UIApplication.shared.keyWindow!.rootViewController
         viewController?.present(playerViewController, animated: true) {
             playerViewController.player!.play()
@@ -28,10 +27,10 @@ func playVideo(url: URL) {
 func saveToLibrary(url: URL?) {
     if let assetUrl = url {
         let startSaveTime = Date()
-        
+
         PHPhotoLibrary.shared().performChanges({
             PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: assetUrl)
-        }) { saved, error in
+        }) { saved, _ in
             if saved {
                 print("----------- \(startSaveTime.timeIntervalSinceNow * -1) seconds for saving to Library")
             }
@@ -39,14 +38,14 @@ func saveToLibrary(url: URL?) {
     }
 }
 
-func append(pixelBufferAdaptor adaptor: AVAssetWriterInputPixelBufferAdaptor, with image: UIImage, at presentationTime: CMTime, success: @escaping (() -> ())) throws {
+func append(pixelBufferAdaptor adaptor: AVAssetWriterInputPixelBufferAdaptor, with image: UIImage, at presentationTime: CMTime, success: @escaping (() -> Void)) throws {
     do {
         if let pixelBufferPool = adaptor.pixelBufferPool {
             let pixelBufferPointer = UnsafeMutablePointer<CVPixelBuffer?>.allocate(capacity: MemoryLayout<CVPixelBuffer?>.size)
             let status: CVReturn = CVPixelBufferPoolCreatePixelBuffer(
-              kCFAllocatorDefault,
-              pixelBufferPool,
-              pixelBufferPointer
+                kCFAllocatorDefault,
+                pixelBufferPool,
+                pixelBufferPointer
             )
             guard let pixelBuffer = pixelBufferPointer.pointee else {
                 return
@@ -54,17 +53,16 @@ func append(pixelBufferAdaptor adaptor: AVAssetWriterInputPixelBufferAdaptor, wi
             guard status == 0 else {
                 return
             }
-            
+
             fill(pixelBuffer: pixelBuffer, with: image)
             if adaptor.append(pixelBuffer, withPresentationTime: presentationTime) {
-                pixelBufferPointer.deinitialize(count:1)
+                pixelBufferPointer.deinitialize(count: 1)
                 success()
-            } else {
-            }
-            
+            } else {}
+
             pixelBufferPointer.deallocate()
         }
-    } catch let error {
+    } catch {
         throw error
     }
 }
@@ -73,16 +71,16 @@ func append(pixelBufferAdaptor adaptor: AVAssetWriterInputPixelBufferAdaptor, wi
 private func fill(pixelBuffer: CVPixelBuffer, with image: UIImage) {
     // lock the buffer memoty so no one can access it during manipulation
     CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
-    
+
     // get the pixel data from the address in the memory
     let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
-    
+
     // create a color scheme
     let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-    
+
     /// set the context size
     let contextSize = image.size
-    
+
     // generate a context where the image will be drawn
     if let context = CGContext(data: pixelData,
                                width: Int(contextSize.width),
@@ -91,30 +89,29 @@ private func fill(pixelBuffer: CVPixelBuffer, with image: UIImage) {
                                bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
                                space: rgbColorSpace,
                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) {
-        
         var imageHeight = image.size.height
         var imageWidth = image.size.width
-        
+
         if Int(imageHeight) > context.height {
             imageHeight = 16 * (CGFloat(context.height) / 16).rounded(.awayFromZero)
         } else if Int(imageWidth) > context.width {
             imageWidth = 16 * (CGFloat(context.width) / 16).rounded(.awayFromZero)
         }
-        
+
         let center = CGPoint.zero
-        
+
         context.clear(CGRect(x: 0.0, y: 0.0, width: imageWidth, height: imageHeight))
-        
+
         // set the context's background color
         context.fill(CGRect(x: 0.0, y: 0.0, width: CGFloat(context.width), height: CGFloat(context.height)))
         context.concatenate(.identity)
-        
+
         // draw the image in the context
-        
+
         if let cgImage = image.cgImage {
             context.draw(cgImage, in: CGRect(x: center.x, y: center.y, width: imageWidth, height: imageHeight))
         }
-        
+
         // unlock the buffer memory
         CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
     }
--- LottieExportDemo/ViewController.swift
+++ LottieExportDemo/ViewController.swift
@@ -6,123 +6,120 @@
 //  Copyright © 2019 Damik Minnegalimov. All rights reserved.
 //
 
-import UIKit
-import Lottie
 import AVKit
-
+import Lottie
+import UIKit
 
 class ViewController: UIViewController {
     var animationView: AnimationView?
     var animation: Animation?
-    
+
     var exportButton: UIButton?
     var oldExportButton: UIButton?
 
     let size = CGSize(width: 1000, height: 1250)
 
     private var videoWriter: AVAssetWriter?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .black
         Animation.loadedFrom(url: URL(string: "https://assets9.lottiefiles.com/packages/lf20_dH29dn.json")!,
                              closure: { animation in self.animationLoaded(newAnimation: animation) },
                              animationCache: nil)
     }
-    
+
     func animationLoaded(newAnimation: Animation?) {
         animationView = AnimationView(animation: newAnimation)
         animationView?.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         animationView?.center = view.center
-        
+
         animationView?.loopMode = .loop
         animationView?.play()
-        
+
         view.addSubview(animationView!)
-        
+
         animation = newAnimation
         startExport()
-        
+
         addButtons()
     }
-    
+
     @objc func startExport() {
         guard let animation = animation else {
             print("Set up Animation first")
             return
         }
-        
+
         let bundleURL = Bundle.main.resourceURL!
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let outputURL = documentsDirectory.appendingPathComponent("processed.mov")
         try? FileManager.default.removeItem(at: outputURL)
-        
+
         let duration = CMTime(seconds: animation.duration, preferredTimescale: 600)
-        
+
         /// Create composition
         let compositionRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         let timerange: CMTimeRange = CMTimeRange(start: .zero, duration: duration)
-        
+
         let composition = AVMutableComposition()
         let videoComposition = AVMutableVideoComposition()
-        
+
         let instructions = AVMutableVideoCompositionInstruction()
         instructions.backgroundColor = UIColor.clear.cgColor
         instructions.timeRange = timerange
-        
-        
+
         /// Create main parent layer for AVVideoCompositionCoreAnimationTool
         let parentLayer = CALayer()
         parentLayer.isGeometryFlipped = true
         parentLayer.frame = compositionRect
-        
+
         let videoCALayer = CALayer()
         videoCALayer.frame = compositionRect
         parentLayer.addSublayer(videoCALayer)
-        
-        
+
         /// Create needed assets and tracks (AVFoundation classes)
-        let avAsset = AVAsset(url: URL(string:"poppets.mov", relativeTo:bundleURL)!)
-        
+        let avAsset = AVAsset(url: URL(string: "poppets.mov", relativeTo: bundleURL)!)
+
         guard let videoTrack = avAsset.tracks(withMediaType: .video).first else {
             NSLog("Error: there is no video track in video")
             return
         }
-        
+
         let compositionVideoTrack = composition.addMutableTrack(withMediaType: .video,
                                                                 preferredTrackID: kCMPersistentTrackID_Invalid)
         try! compositionVideoTrack?.insertTimeRange(timerange, of: videoTrack, at: .zero)
-        
+
         /// Set up effects for current layer (video)
         let layerIntruction = AVMutableVideoCompositionLayerInstruction(assetTrack: compositionVideoTrack!)
         layerIntruction.setOpacity(0, at: .zero)
-        
+
         /// Add effects to global instructions
         instructions.layerInstructions.append(layerIntruction)
-        
+
         /// Add Lottie
         addLottieLayer(animation: animation, to: parentLayer, with: compositionRect)
-        
+
         /// Set up composition size and framerate
         videoComposition.instructions = [instructions]
-        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: Int32(animation.framerate));
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: Int32(animation.framerate))
         videoComposition.renderSize = size
         videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoCALayer, in: parentLayer)
-        
+
         /// Run export
         export(
             composition: composition,
             videoComposition: videoComposition,
-            outputUrl: outputURL) { outputURL in
-                playVideo(url: outputURL)
+            outputUrl: outputURL
+        ) { outputURL in
+            playVideo(url: outputURL)
         }
     }
-    
+
     func addLottieLayer(animation: Animation,
-                   to layer: CALayer,
-                   with frame: CGRect) {
-        
+                        to layer: CALayer,
+                        with frame: CGRect) {
         let animationView = AnimationView()
         animationView.animation = animation
         animationView.loopMode = .loop
@@ -132,11 +129,11 @@ class ViewController: UIViewController {
         let animationLayer = animationView.layer
         animationLayer.frame = frame
         animationLayer.layoutSublayers()
-        
+
         layer.addSublayer(animationLayer)
         animationView.play()
     }
-    
+
     func export(composition: AVMutableComposition,
                 videoComposition: AVMutableVideoComposition,
                 outputUrl: URL,
@@ -146,128 +143,127 @@ class ViewController: UIViewController {
             exportSession.outputURL = outputUrl
             exportSession.timeRange = CMTimeRangeMake(start: .zero, duration: composition.duration)
             exportSession.outputFileType = .mov
-            
-            self.toggleExportButton(needEnable: false)
-            
+
+            toggleExportButton(needEnable: false)
+
             let startTime = Date()
             exportSession.exportAsynchronously {
                 print("- \(startTime.timeIntervalSinceNow * -1) seconds elapsed for AVAssetExportSession")
-                
+
                 self.toggleExportButton(needEnable: true)
-                
+
                 if exportSession.status.rawValue == 4 {
                     print("Export failed -> Reason: \(exportSession.error!.localizedDescription))")
                     print(exportSession.error!)
                     return
                 }
-                
+
                 completion?(outputUrl)
             }
         }
     }
-    
+
     @objc func oldExport() throws {
-        self.toggleOldExportButton(needEnable: false)
+        toggleOldExportButton(needEnable: false)
 
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
         let outputURL = documentsDirectory.appendingPathComponent("processed.mov")
         try? FileManager.default.removeItem(at: outputURL)
-        
+
         let fps = Int64(animation?.framerate ?? 30)
         var framesMax = CGFloat(fps)
-                        
+
         if let animation = animation {
             framesMax = CGFloat(animation.duration * Double(fps))
         }
-        
+
         animationView?.loopMode = .playOnce
         animationView?.stop()
-                
+
         /*
          * Set up VideoWriter
          */
         do {
             try videoWriter = AVAssetWriter(outputURL: outputURL, fileType: AVFileType.mov)
-        } catch let error {
-            throw(error)
+        } catch {
+            throw (error)
         }
-        
+
         guard let videoWriter = videoWriter else {
             return
         }
-        
-        let videoSettings: [String : Any] = [
-            AVVideoCodecKey : AVVideoCodecType.h264,
-            AVVideoWidthKey : size.width,
-            AVVideoHeightKey : size.height,
+
+        let videoSettings: [String: Any] = [
+            AVVideoCodecKey: AVVideoCodecType.h264,
+            AVVideoWidthKey: size.width,
+            AVVideoHeightKey: size.height,
         ]
-        
+
         let videoWriterInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: videoSettings)
-        
-        let sourceBufferAttributes: [String : Any] = [
-            (kCVPixelBufferPixelFormatTypeKey as String): Int(kCVPixelFormatType_32ARGB),
-            (kCVPixelBufferWidthKey as String): Float(size.width),
-            (kCVPixelBufferHeightKey as String): Float(size.height)
+
+        let sourceBufferAttributes: [String: Any] = [
+            kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
+            kCVPixelBufferWidthKey as String: Float(size.width),
+            kCVPixelBufferHeightKey as String: Float(size.height),
         ]
-        
+
         let pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: videoWriterInput,
                                                                       sourcePixelBufferAttributes: sourceBufferAttributes)
-        
+
         assert(videoWriter.canAdd(videoWriterInput))
         videoWriter.add(videoWriterInput)
-        
+
         if videoWriter.startWriting() {
             let startTime = Date()
             videoWriter.startSession(atSourceTime: CMTime.zero)
             assert(pixelBufferAdaptor.pixelBufferPool != nil)
-            
+
             let writeQueue = DispatchQueue(label: "writeQueue", qos: .userInteractive)
-            
+
             videoWriterInput.requestMediaDataWhenReady(on: writeQueue, using: {
                 let frameDuration = CMTimeMake(value: 1, timescale: Int32(fps))
                 var frameCount: Int64 = 0
-                
+
                 /*
                  * Start render loop
                  */
-                while(Int(frameCount) < Int(framesMax)) {
+                while Int(frameCount) < Int(framesMax) {
                     if videoWriterInput.isReadyForMoreMediaData {
                         DispatchQueue.main.sync {
                             let lastFrameTime = CMTimeMake(value: frameCount, timescale: Int32(fps))
                             let presentationTime = frameCount == 0 ? lastFrameTime : CMTimeAdd(lastFrameTime, frameDuration)
-                            
+
                             // Set up Lottie
                             self.animationView?.currentProgress = CGFloat(frameCount) / framesMax
 
                             UIGraphicsBeginImageContextWithOptions(self.size, false, 0.0)
-                            
+
                             if let animationView = self.animationView {
                                 animationView.drawHierarchy(in: animationView.frame,
                                                             afterScreenUpdates: false)
                             }
-                            
+
                             let image = UIGraphicsGetImageFromCurrentImageContext()
                             UIGraphicsEndImageContext()
-                            
+
                             do {
                                 try append(pixelBufferAdaptor: pixelBufferAdaptor,
-                                                with: image!,
-                                                at: presentationTime,
-                                                success: {
-                                                    frameCount += 1
+                                           with: image!,
+                                           at: presentationTime,
+                                           success: {
+                                               frameCount += 1
                                 })
-                            } catch {
-                            } // Do not throw here
+                            } catch {} // Do not throw here
                         }
                     }
                 }
-                
+
                 videoWriterInput.markAsFinished()
-                
+
                 videoWriter.finishWriting {
                     print("--- \(startTime.timeIntervalSinceNow * -1) seconds elapsed for AVAssetWriterInput")
-                    
+
                     self.toggleOldExportButton(needEnable: true)
 
                     self.animationView?.loopMode = .loop
@@ -285,36 +281,35 @@ extension ViewController {
             self.exportButton?.isEnabled = needEnable
         }
     }
-    
+
     func toggleOldExportButton(needEnable: Bool) {
         DispatchQueue.main.async {
             self.oldExportButton?.setTitle(needEnable ? "Start AVAssetWriter" : "Processing...", for: .normal)
             self.oldExportButton?.isEnabled = needEnable
         }
     }
-    
+
     func addButtons() {
         let buttonSize = CGSize(width: 250, height: 40)
-            
+
         /// Start AVAssetExportSession button
         exportButton = UIButton(frame: CGRect(origin: .zero, size: buttonSize))
-        
+
         exportButton?.setTitle("Start AVAssetExportSession", for: .normal)
         exportButton?.addTarget(self, action: #selector(startExport), for: .touchUpInside)
-        
+
         exportButton?.center = view.center
         exportButton?.frame.origin.y = exportButton!.frame.origin.y + 190
         view.addSubview(exportButton!)
-        
+
         /// Start AVAssetWriter button
         oldExportButton = UIButton(frame: CGRect(origin: .zero, size: buttonSize))
-        
+
         oldExportButton?.setTitle("Start AVAssetWriter", for: .normal)
         oldExportButton?.addTarget(self, action: #selector(oldExport), for: .touchUpInside)
-        
+
         oldExportButton?.center = view.center
         oldExportButton?.frame.origin.y = oldExportButton!.frame.origin.y + 240
         view.addSubview(oldExportButton!)
     }
 }
-
